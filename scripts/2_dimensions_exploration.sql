4a0642c2824f913a37e0a4671ce3c19bc8ec454c
Ahmed Maghrabi <ahmedmaghrabi328@gmail.com>
2026-08-25 10:47:31 +0300

    Add SQL queries for customer and product exploration

---
+++ scripts/2_dimensions_exploration.sql
@@ -0,0 +1,29 @@
+/*
+===============================================================================
+Dimensions Exploration
+===============================================================================
+Purpose:
+    - To explore the structure of dimension tables.
+	
+SQL Functions Used:
+    - DISTINCT
+    - ORDER BY
+===============================================================================
+*/
+
+-- Explore All countries our customers come from.
+SELECT DISTINCT
+	country
+FROM gold.dim_customers
+ORDER BY country;
+
+-- Explore All categories 'The major Divisions'
+SELECT DISTINCT
+	category,
+	subcategory,
+	product_name
+FROM gold.dim_products
+ORDER BY 	
+	category,
+	subcategory,
+	product_name
